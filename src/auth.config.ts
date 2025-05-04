da422b1630879a3995ef33adc8b833ad7ba8aca5
Daniel González Briseño <qbixmex@gmail.com>
2025-05-03 18:43:35 -0600

    Prisma Adapter

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,10 @@
-// import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import prisma from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID ?? undefined,
