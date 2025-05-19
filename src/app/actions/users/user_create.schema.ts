ef4e331454d102d93ac1190c6e2b0febe943315f
Daniel González Briseño <qbixmex@gmail.com>
2025-05-19 11:49:55 -0600

    Update User
    - Updated User Form
    - Added User Create Schema
    - Added User Update Schema
    - Translate to Spanish some texts

--- src/app/actions/users/user_create.schema.ts
+++ src/app/actions/users/user_create.schema.ts
@@ -1,9 +1,11 @@
+import { Role } from "@/app/actions/users/role.enum";
 import { z } from "zod";
 
-const usersCreateSchema = z.object({
+const userCreateSchema = z.object({
   name: z.string().min(3, { message: "Name must contains at least 3 characters" }),
   email: z.string().email({ message: "Email is invalid" }),
   password: z.string().min(6, { message: "Password must contains at least 6 characters" }),
+  roles: z.array(z.nativeEnum(Role)).optional(),
 });
 
-export default usersCreateSchema;
+export default userCreateSchema;
--- src/app/admin/users/(components)/form.tsx
+++ src/app/admin/users/(components)/form.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import { FC } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -14,37 +15,59 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
-import usersSchema from "@/app/actions/users/users.schema";
+import userUpdateSchema from "./user_update.schema";
+import userCreateSchema from "./user_create.schema";
 import createUser from "@/app/actions/users/create_user";
 import { useRouter } from "next/navigation";
+import { User } from "@/interfaces/user.interface";
+import { Role } from "@/app/actions/users/role.enum";
+import updateUser from "@/app/actions/users/update_user";
+import Link from "next/link";
 
-export const UserForm = () => {
+type Props = Readonly<{
+  user?: User | null;
+}>;
+
+type FormValues = z.infer<typeof userUpdateSchema> | z.infer<typeof userCreateSchema>;
+
+export const UserForm: FC<Props> = ({ user }) => {
   const router = useRouter();
 
-  const form = useForm<z.infer<typeof usersSchema>>({
-    resolver: zodResolver(usersSchema),
+  const form = useForm<FormValues>({
+    resolver: zodResolver(user ? userUpdateSchema : userCreateSchema),
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
+      name: user?.name ?? "",
+      email: user?.email ?? "",
+      password: user?.password ?? "",
+      roles: user?.roles as Role[] ?? [],
       passwordConfirmation: "",
     },
   });
 
-  const onSubmit = async (data: z.infer<typeof usersSchema>) => {
+  const onSubmit = async (data: FormValues) => {
     const formData = new FormData();
     formData.append("name", data.name);
     formData.append("email", data.email);
-    formData.append("password", data.password);
+    formData.append("password", data.password ?? "");
 
-    const result = await createUser(formData);
+    const response = user && user.id
+      ? await updateUser(user.id, formData)
+      : await createUser(formData)
 
-    if (result.ok) {
-      toast.success(result.message);
+    if (!response.ok) {
+      toast.error(response.message, {
+        duration: 3000,
+        position: "top-right",
+        className: "bg-red-500 text-white",
+      });
     }
 
-    if (!result.ok) {
-      toast.error(result.message);
+    if (response.ok) {
+      toast.success(response.message, {
+        duration: 3000,
+        position: "top-right",
+        className: "bg-green-500 text-white",
+      });
     }
 
     form.reset();
@@ -62,7 +85,7 @@ export const UserForm = () => {
             name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Name</FormLabel>
+                <FormLabel>Nombre</FormLabel>
                 <FormControl>
                   <Input {...field} />
                 </FormControl>
@@ -76,7 +99,7 @@ export const UserForm = () => {
             name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
+                <FormLabel>Correo Electrónico</FormLabel>
                 <FormControl>
                   <Input {...field} />
                 </FormControl>
@@ -90,7 +113,7 @@ export const UserForm = () => {
             name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Password</FormLabel>
+                <FormLabel>Contraseña</FormLabel>
                 <FormControl>
                   <Input type="password" {...field} />
                 </FormControl>
@@ -104,21 +127,25 @@ export const UserForm = () => {
             name="passwordConfirmation"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Repeat Password</FormLabel>
+                <FormLabel>Repite Contraseña</FormLabel>
                 <FormControl>
                   <Input type="password" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
-          />          
+          />
 
-          <div className="w-full lg:flex lg:justify-end">
+          <div className="w-full lg:flex gap-3 lg:justify-end">
+            <Link
+              href="/admin/users"
+              className={buttonVariants({ variant: 'outline' })}
+            >Cancel</Link>
             <Button
               type="submit"
               variant="primary"
               className="w-full lg:w-auto"
-            >Submit</Button>
+            >Guardar</Button>
           </div>
         </div>
       </form>
